72b12c62db9351599247df71d83400f5babcd9c4
LucaTaglialatela <luca.taglialatela@gmail.com>
2020-03-04 11:27:20 +0100

    added Game/Engine/UIHandler skeletons + functions implementations

---
+++ src/main/java/Game.java
@@ -0,0 +1,33 @@
+import Entities.GameEntities;
+import Entities.Initializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private String gameOverItem;
+    private Boolean playerWon = false;
+    private List<String> previousCommands = new ArrayList<>();
+
+    GameEntities gameEntities = Initializer.loadGameFiles();
+
+//    Boolean isGameOver() {
+//        return gameEntities.getPlayer().has(gameOverItem); // TODO: have has function implemented somewhere
+//    }
+
+    List<String> getPreviousCommands() { return previousCommands; }
+
+    String handleCommand(String input) {
+        String[] args = input.split(" ", 0);
+        if(args.length == 2) {
+//            System.out.println(args[0] + " " + args[1] + " player");
+            previousCommands.add(input);
+        } else if(args.length == 3) {
+//            System.out.println(args[0] + " " + args[1] + " " + args[2]);
+            previousCommands.add(input);
+        } else {
+            System.out.println("Invalid command\n");
+        }
+        return input;
+    } // handle command and append to previousCommands return state to user as string
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,22 @@ import Entities.Initializer;
 import Entities.GameEntities;
 import Entities.Item;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main (String[] args){
-        //System.out.println("Welcome to Software Design");
+//        System.out.println("Welcome to Software Design");
         GameEntities gameEntities = Initializer.loadGameFiles();
+        Engine engine = new Engine();
+
+        engine.runGame();
+
         //System.out.println(gameEntities.getAreaEntities().get("forest").connectsTo("north"));
     }
 }
