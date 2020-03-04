b27f53921fd9cd5f08e33c23bfece41fb6f80d0c
LucaTaglialatela <luca.taglialatela@gmail.com>
2020-03-04 11:27:20 +0100

    added Game/Engine/UIHandler skeletons + functions implementations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,17 @@ import booster.init.GameEntities;
 import booster.init.EntityInitializer;
 //import entity.Command;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main (String[] args){
+
         //System.out.println("Welcome to Software Design");
         GameEntities gameEntities = EntityInitializer.loadGameFiles();
 
---
+++ src/main/java/UIHandler.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UIHandler {
+    private Scanner scanner = new Scanner(System.in);
+
+//    void displayGamesMenu(List<String> games) {}
+
+//    void displayMainMenu() {}
+
+//    void displayGameSubMenu() {}
+
+    String getInput() { return scanner.nextLine(); }
+
+//    void clearScreen() {}
+
+//    void printResult(String result) {}
+}
