f6c404dafeb22a383be200935274dcdf7269f000
a-wilkes <a.sjwilkes@gmail.com>
2020-03-08 19:10:32 +0100

    Implement DropItem

--- src/main/java/command/DropItem.java
+++ src/main/java/command/DropItem.java
@@ -1,4 +1,34 @@
 package command;
 
-public class DropItem {
+import dictionary.GameEntities;
+import entity.Area;
+import entity.ICharacter;
+import entity.IInteractable;
+
+import java.util.Set;
+
+public class DropItem extends Command {
+    private IInteractable parent;
+    private Set<String> args;
+
+    public DropItem(final Set<String> args, final IInteractable parent) {
+        this.args = args;
+        this.parent = parent;
+    }
+
+    @Override
+    public String apply(String object, GameEntities ge) {
+        if (!(parent instanceof ICharacter)) { return "You cannot do that."; }
+
+        ICharacter p = (ICharacter) parent;
+
+        if (!p.hasInInventory(object)) { return String.format("You are not holding '%s'", object); }
+        p.removeFromInventory(object);
+
+        Area area = ge.getArea(parent.getCurrentLocation());
+
+        area.addToInventory(object);
+
+        return object + " -> " + area.getName();
+    }
 }
