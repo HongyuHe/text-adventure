64575e6f19b01c51a89830e9b6edbd46df48bf2e
a-wilkes <a.sjwilkes@gmail.com>
2020-03-08 19:11:31 +0100

    Update ListContents to handle casting and errors more robustly

--- src/main/java/command/ListContents.java
+++ src/main/java/command/ListContents.java
@@ -1,12 +1,14 @@
 package command;
 
 import dictionary.GameEntities;
+import entity.Entity;
+import entity.ICharacter;
 import entity.IInteractable;
 
 import java.util.Set;
 
 public class ListContents extends Command {
-    private IInteractable parent; // NOSONAR
+    private IInteractable parent;
     private Set<String> args;
 
     public ListContents(final Set<String> args, final IInteractable parent) {
@@ -15,7 +17,18 @@ public class ListContents extends Command {
     }
 
     @Override
-    public String apply(final String object, final GameEntities ge) {
-        return "Action>>>$ "+ "List contents of " + args;
+    public String apply(final String object, final GameEntities ge)
+    {
+        if (!(parent instanceof ICharacter)) { return "You cannot do that."; }
+
+        final ICharacter p = (ICharacter) parent;
+
+        StringBuilder contents = new StringBuilder("Contents: ");
+
+        for (final String item : p.getInventory()) { contents.append(item).append(", "); }
+
+        if (!p.getInventory().isEmpty()) { contents.delete(contents.length() - 2, contents.length()); }
+
+        return contents.toString();
     }
 }
