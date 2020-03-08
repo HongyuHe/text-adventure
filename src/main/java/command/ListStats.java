35f703862c3d8b5556a8de978c517e054bfe0e38
a-wilkes <a.sjwilkes@gmail.com>
2020-03-08 22:33:48 +0100

    Implement ListStats

--- src/main/java/command/ListStats.java
+++ src/main/java/command/ListStats.java
@@ -1,4 +1,33 @@
 package command;
 
-public class ListStats {
+import cork.Initializer;
+import dictionary.GameEntities;
+import entity.ICharacter;
+import entity.IInteractable;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ListStats extends Command {
+    private IInteractable parent;
+    private Set<String> args;
+
+    public ListStats(final Set<String> args, final IInteractable parent) {
+        this.args = args;
+        this.parent = parent;
+    }
+
+    @Override
+    public String apply(final String object, final GameEntities ge)
+    {
+        if (!(parent instanceof ICharacter)) { return "You cannot do that."; }
+
+        final ICharacter p = (ICharacter) parent;
+
+        StringBuilder contents = new StringBuilder("Stats:\n");
+
+        for (final Map.Entry<String, Integer> stat : p.getStats().entrySet()) { contents.append(String.format("\t%s: %d%n", stat.getKey(), stat.getValue())); }
+
+        return contents.toString();
+    }
 }
