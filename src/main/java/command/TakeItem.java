e40d194e695e6d9f19d775f76e12f2fc43104699
a-wilkes <a.sjwilkes@gmail.com>
2020-03-08 19:10:45 +0100

    Implement TakeItem

--- src/main/java/command/TakeItem.java
+++ src/main/java/command/TakeItem.java
@@ -1,4 +1,33 @@
 package command;
 
-public class TakeItem {
+import dictionary.GameEntities;
+import entity.Area;
+import entity.ICharacter;
+import entity.IInteractable;
+
+import java.util.Set;
+
+public class TakeItem extends Command {
+    private IInteractable parent;
+    private Set<String> args;
+
+    public TakeItem(final Set<String> args, final IInteractable parent) {
+        this.args = args;
+        this.parent = parent;
+    }
+
+    @Override
+    public String apply(String object, GameEntities ge) {
+        if (!(parent instanceof ICharacter)) { return "You cannot do that."; }
+
+        Area area = ge.getArea(parent.getCurrentLocation());
+
+        if (!area.hasInInventory(object)) { return String.format("You cannot pick up '%s'", object); }
+
+        area.removeFromInventory(object);
+
+        ((ICharacter) parent).addToInventory(object);
+
+        return object + " -> " + parent.getName();
+    }
 }
